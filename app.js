40835c069715c850fc0bd4bb03c251a0d81154b9
chatmongcol <cdadwngs@gmail.com>
2022-10-27 16:34:28 +0700

    update

---
+++ app.js
@@ -0,0 +1,9 @@
+ const app = Vue.createApp({
+    data(){
+        return{
+             message: "Hello World!",
+             fname: "Mark Zackerberg",
+             count: 0,
+        }
+    }
+ }).mount("#app")
